086386722f151e1e61d2a7b1d75f63282d1bba1a
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-10-30 13:10:44 -0400

    Initial code review.
    Added comments based on breaking down starter code.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,41 @@
 // Assignment code here
+var length_criteria = 0
+var minimum_length = 8
+var maximum_length = 128
+var number_of_character_types_selected = 0
+var minimum_character_types = 1
+var character_types = ["lowercase", "uppercase", "numeric", "special"]
+var character_type_selected = [false, false, false, false]
 
-
-// Get references to the #generate element
+// Get references to the #generate html element, used for the event listener
 var generateBtn = document.querySelector("#generate");
+
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // trigger function to generate the password
+  var password = generatePassword();
+  // grab the password readonly text area
+  var displayPasswordText = document.querySelector("#password");
+  // display the generated password in the #password html element
+  displayPasswordText.value = password;
 
 }
 
-// Add event listener to generate button
+
+
+// function to generate password
+function generatePassword() {
+
+}
+
+
+
+
+
+
+// Add event listener to generate button, triggers the writePassword function when clicked
 generateBtn.addEventListener("click", writePassword);
