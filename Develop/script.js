5a8a11ee57a00b0d25a1932a71e5df7b03646c47
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-10-30 16:09:42 -0400

    Declared all input variables.
    Run through prompts to confirm y/n for length and character types. Validate the input to ensure only y/n are processed.

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,11 @@ var minimum_length = 8
 var maximum_length = 128
 var number_of_character_types_selected = 0
 var minimum_character_types = 1
-var character_types = ["lowercase", "uppercase", "numeric", "special"]
-var character_type_selected = [false, false, false, false]
+// var character_types = ["lowercase", "uppercase", "numeric", "special"]
+var lowercaseSelected = false;
+var uppercaseSelected = false;
+var numericTypeSelected = false;
+var specialTypeSelected = false;
 
 // Get references to the #generate html element, used for the event listener
 var generateBtn = document.querySelector("#generate");
@@ -18,8 +21,10 @@ function writePassword() {
 
   // trigger function to generate the password
   var password = generatePassword();
+
   // grab the password readonly text area
   var displayPasswordText = document.querySelector("#password");
+
   // display the generated password in the #password html element
   displayPasswordText.value = password;
 
@@ -46,17 +51,94 @@ function generatePassword() {
       // rerun the function to get the length
       getLength();
     }
-  }
-
-
-  function getCharacterTypes() {
     
   }
 
 
-getLength();
-getCharacterTypes(); 
+  function getLowercase() {
+    let lowercasePrompt = prompt('Would you like to include lowercase letters? (y/n)');
+    // console.log(lowercasePrompt);
+
+    // if answer is not y or n, rerun the function.
+    if (lowercasePrompt != 'y' && lowercasePrompt != 'n') {
+    alert("Please enter 'y' or 'n'.");
+    getLowercase();
+  }
+    // set variable to true if y
+    if (lowercasePrompt === 'y') {
+      lowercaseSelected = true
+    }
+
+    // console.log("Lowercase:" + lowercaseSelected); 
+  }
+
+  function getUppercase() {
+    let uppercasePrompt = prompt('Would you like to include uppercase letters? (y/n)');
+    // console.log(uppercasePrompt);
+
+    // if answer is not y or n, rerun the function.
+    if (uppercasePrompt != 'y' && uppercasePrompt != 'n') {
+      alert("Please enter 'y' or 'n'.");
+      getUppercase();
+    }
+    // set variable to true if y
+    if (uppercasePrompt === 'y') {
+      uppercaseSelected = true
+    };
+    // console.log("Uppercase:" + uppercaseSelected);
+  }  
+
+  function getNumbericType() {
+    let numericTypePrompt = prompt('Would you like to include numbers? (y/n)');
+    // console.log(numericTypePrompt);
+
+    // if answer is not y or n, rerun the function.
+    if (numericTypePrompt != 'y' && numericTypePrompt != 'n') {
+      alert("Please enter 'y' or 'n'.");
+      getNumbericType();
+    }
+    // set variable to true if y
+    if (numericTypePrompt === 'y') {
+      numericTypeSelected = true
+    };
+    // console.log("Numbers: " + numericTypeSelected);
+  }
+
+  function getSpecialType() {
+    let specialTypePrompt = prompt('Would you like to include special characters? (y/n)');
+    // console.log(specialTypePrompt);
+
+    // if answer is not y or n, rerun the function.
+    if (specialTypePrompt != 'y' && specialTypePrompt != 'n') {
+      alert("Please enter 'y' or 'n'.");
+      getSpecialType();
+    }
+    // set variable to true if y
+    if (specialTypePrompt === 'y') {
+      specialTypeSelected = true
+    };
+    // console.log("Special Characaters:" + specialTypeSelected);
+  }
+
+  
+  function generateRandomPassword() {
+
+  }
+  
+  
+
+  
+  //trigger functions
+  getLength();
+  getLowercase();
+  getUppercase();
+  getNumbericType();
+  getSpecialType();
+  generateRandomPassword();
+
 }
+
+
 
 
 // Add event listener to generate button, triggers the writePassword function when clicked
